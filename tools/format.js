cb442bd4d6796e2b22e2e88dd393ee612febf804
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-11-10 14:21:02 +0100

    users -> people

--- tools/format.js
+++ tools/format.js
@@ -30,6 +30,6 @@ const mapUserFields = ({
   nationality: nationalities[nat]
 })
 
-const users = results.map(mapUserFields);
+const people = results.map(mapUserFields);
 const filePath = path.resolve(__dirname, '../db.json');
-fs.writeFileSync(filePath, JSON.stringify({ users }, null, 2));
+fs.writeFileSync(filePath, JSON.stringify({ people }, null, 2));
